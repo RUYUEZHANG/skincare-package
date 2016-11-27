19ef8bb784c9c402fd5e83722d1ca7d180a03171
Ruyue Zhang <Ruyue_Zhang@urmc.rochester.edu>
2016-11-26 20:35:24 -0500

    finished the recodecatvar function

--- R/recodecatvar.R
+++ R/recodecatvar.R
@@ -1,9 +1,11 @@
 #format character variables into numerical
 #x is the number of category
+#d is the dataset
 #y is the column you want to recode
 #z is the vector of category names
-recodecatvar <- function(x,y,z) {
+recodecatvar <- function(x,d,y,z) {
   for (i in 1:x){
-    y<-gsub(z[1],i,y,fixed=TRUE)
+    d[,y]<-gsub(z[i],i,d[,y],fixed=TRUE)
   }
+  return(d)
 }
