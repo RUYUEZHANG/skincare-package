c44d0bfea8f5ba473f755329074981d1e29ceb56
Ruyue Zhang <Ruyue_Zhang@urmc.rochester.edu>
2016-11-26 23:15:15 -0500

    finished the first version of multittest function, added description words in front of each return. also tested it with mtcars data.

--- R/multittest.R
+++ R/multittest.R
@@ -1,11 +1,16 @@
 #do t-test for several variables
-#x is a dataframe
+#x is a dataset
 #y is a variable
 #z is a character vector contains the column names you want to do t-test with y
 #the data requires to be formatted
 multittest<-function(x,y,z) {
-  n<-ncol(z)
-  for (i in 1:n )
-  print(t.test(y,x$z[i]))
-
+  z<-as.matrix(z)
+  n<-nrow(z)
+  title<-"t.test between"
+  for (i in 1:n ){
+  ob1<-(colnames(x)[y])
+  ob2<-(colnames(x)[z[i]])
+  print(paste(title,ob1,"and",ob2,sep=" "))
+  print(t.test(x[,y],x[,z[i]]))
+}
 }
